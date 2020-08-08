22759dcfc6c7d5f1c1ad1af9391ea7e66a2f1cd6
Ian J Sikes <ianjsikes@gmail.com>
2020-08-08 16:20:08 -0700

    Revert "Experimental react-three-fiber test"
    This reverts commit 793a617d9515dd58ce50cd5d56f8a2399362b417.

--- webview/src/Graph.tsx
+++ webview/src/Graph.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import * as d3 from 'd3'
-import * as THREE from 'three'
-import { Canvas, useFrame } from 'react-three-fiber'
 import { D3Node, State } from './types'
 import { useWindowSize, tickUntilDone } from './utils'
 import Node from './Node'
 import Edge from './Edge'
 import ZoomContainer from './Zoom'
 import { useGraphState } from './vscode'
-
-const backgroundColor = getComputedStyle(
-  document.documentElement
-).getPropertyValue('--vscode-editor-foreground')
-const bg = new THREE.Color(backgroundColor)
 
 interface Props {}
 
@@ -28,8 +21,7 @@ const Graph: React.FC<Props> = () => {
       .forceSimulation<D3Node>()
       .force(
         'center',
-        d3.forceCenter<D3Node>(0, 0)
-        // d3.forceCenter<D3Node>((width || 0) / 2, (height || 0) / 2)
+        d3.forceCenter<D3Node>((width || 0) / 2, (height || 0) / 2)
       )
       .force(
         'link',
@@ -51,8 +43,7 @@ const Graph: React.FC<Props> = () => {
   React.useEffect(() => {
     simulation.force(
       'center',
-      d3.forceCenter(0, 0)
-      // d3.forceCenter((width || 0) / 2, (height || 0) / 2)
+      d3.forceCenter((width || 0) / 2, (height || 0) / 2)
     )
   }, [width, height])
 
@@ -83,140 +74,73 @@ const Graph: React.FC<Props> = () => {
   } else {
     console.log('rendering SOME nodes', Date.now())
   }
-  const w = width || 0
-  const h = height || 0
-  const texture = React.useMemo(
-    () =>
-      new THREE.TextureLoader().load(
-        'https://blog.fastforwardlabs.com/images/2018/02/circle_aa-1518730700478.png'
-      ),
-    []
-  )
-  const frustumSize = 100
-  const aspect = w / h
 
   return (
     <div>
-      <Canvas
-        style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
-        colorManagement
-        orthographic
-        camera={{
-          left: (frustumSize * aspect) / -2,
-          right: (frustumSize * aspect) / 2,
-          top: frustumSize / 2,
-          bottom: frustumSize / -2,
-          far: 100,
-          near: 0.1,
-        }}
-      >
-        {/* <orthographicCamera
-          args={[
-            -(width || 0) / 2,
-            (width || 0) / 2,
-            (height || 0) / 2,
-            -(height || 0) / 2,
-            1,
-            1000,
-          ]}
-        /> */}
-        {/* <mesh visible position={[-30.5, 0, -60]}>
-          <circleBufferGeometry attach="geometry" args={[100, 16]} />
-          <meshBasicMaterial attach="material" color="green" />
-        </mesh> */}
-        <mesh visible position={[0, 0, -5]}>
-          <circleBufferGeometry attach="geometry" args={[30, 16]} />
-          <meshBasicMaterial attach="material" color="green" />
-        </mesh>
-        <group>
-          {nodes.map((node) => {
+      <svg ref={(s) => setSvgRef(s)} width={width} height={height}>
+        <ZoomContainer
+          ref={zoomRef}
+          svg={svgRef}
+          scaleMin={0.2}
+          scaleMax={3}
+          render={({ k }) => {
             return (
-              <Node
-                key={`node-${node.id}`}
-                node={node}
-                active={node.id === state.currentNode}
-                zoomLevel={1}
-              />
+              <>
+                <g className="links">
+                  {edges.map((edge) => (
+                    <Edge
+                      key={`${(edge.source as any).id}-${
+                        (edge.target as any).id
+                      }`}
+                      edge={edge as any}
+                      zoomLevel={k}
+                    />
+                  ))}
+                </g>
+                <g className="nodes">
+                  {nodes.map((node) => (
+                    <Node
+                      key={`node-${node.id}`}
+                      node={node}
+                      active={node.id === state.currentNode}
+                      zoomLevel={k}
+                    />
+                  ))}
+                </g>
+              </>
             )
-            // console.log('node', node.x, node.y)
-            // return (
-            // <mesh key={node.id} visible position={[node.x!, node.y!, -10]}>
-            //   <circleBufferGeometry attach="geometry" args={[10, 16]} />
-            //   <meshBasicMaterial attach="material" color="indianred" />
-            // </mesh>
-            // )
-          })}
-        </group>
-      </Canvas>
+          }}
+        />
+      </svg>
+      <div className="bottomRightContainer">
+        <span>
+          <span id="files">0</span> files
+        </span>
+        <span>
+          <span id="connections">0</span> links
+        </span>
+        <span>
+          <span id="zoom">1.00</span>x
+        </span>
+        <div id="mode-select">
+          <button
+            id="mode-all"
+            className={mode === 'ALL' ? 'active' : undefined}
+            onClick={() => setMode('ALL')}
+          >
+            ALL
+          </button>
+          <button
+            id="mode-focus"
+            className={mode === 'FOCUS' ? 'active' : undefined}
+            onClick={() => setMode('FOCUS')}
+          >
+            FOCUS
+          </button>
+        </div>
+      </div>
     </div>
   )
-
-  // return (
-  //   <div>
-  //     <svg ref={(s) => setSvgRef(s)} width={width} height={height}>
-  //       <ZoomContainer
-  //         ref={zoomRef}
-  //         svg={svgRef}
-  //         scaleMin={0.2}
-  //         scaleMax={3}
-  //         render={({ k }) => {
-  //           return (
-  //             <>
-  //               <g className="links">
-  //                 {edges.map((edge) => (
-  //                   <Edge
-  //                     key={`${(edge.source as any).id}-${
-  //                       (edge.target as any).id
-  //                     }`}
-  //                     edge={edge as any}
-  //                     zoomLevel={k}
-  //                   />
-  //                 ))}
-  //               </g>
-  //               <g className="nodes">
-  //                 {nodes.map((node) => (
-  //                   <Node
-  //                     key={`node-${node.id}`}
-  //                     node={node}
-  //                     active={node.id === state.currentNode}
-  //                     zoomLevel={k}
-  //                   />
-  //                 ))}
-  //               </g>
-  //             </>
-  //           )
-  //         }}
-  //       />
-  //     </svg>
-  //     <div className="bottomRightContainer">
-  //       <span>
-  //         <span id="files">0</span> files
-  //       </span>
-  //       <span>
-  //         <span id="connections">0</span> links
-  //       </span>
-  //       <span>
-  //         <span id="zoom">1.00</span>x
-  //       </span>
-  //       <div id="mode-select">
-  //         <button
-  //           id="mode-all"
-  //           className={mode === 'ALL' ? 'active' : undefined}
-  //           onClick={() => setMode('ALL')}
-  //         >
-  //           ALL
-  //         </button>
-  //         <button
-  //           id="mode-focus"
-  //           className={mode === 'FOCUS' ? 'active' : undefined}
-  //           onClick={() => setMode('FOCUS')}
-  //         >
-  //           FOCUS
-  //         </button>
-  //       </div>
-  //     </div>
-  //   </div>
-  // )
 }
 
 export default Graph
--- webview/src/Node.tsx
+++ webview/src/Node.tsx
@@ -1,13 +1,7 @@
-import React, { Suspense } from 'react'
-import * as THREE from 'three'
-import font from 'three/examples/fonts/helvetiker_regular.typeface.json'
-import { Canvas, useFrame, useLoader } from 'react-three-fiber'
-// import { animated, useSpring } from 'react-spring'
-import { useSpring } from '@react-spring/core'
-import { a } from '@react-spring/three'
+import React from 'react'
+import { animated, useSpring } from 'react-spring'
 import { D3Node } from './types'
 import { clampedZoom } from './utils'
-import Label from './Label'
 
 const inactiveColor = getComputedStyle(
   document.documentElement
@@ -15,8 +9,6 @@ const inactiveColor = getComputedStyle(
 const activeColor = getComputedStyle(document.documentElement).getPropertyValue(
   '--vscode-textLink-foreground'
 )
-
-const FONT = new THREE.Font(font)
 
 interface Props {
   node: D3Node
@@ -33,57 +25,32 @@ const Node: React.FC<Props> = (props) => {
   const fontSize = Math.max(Math.round(clampedZoom(14, props.zoomLevel)), 1)
   const labelOffset = clampedZoom(15, props.zoomLevel)
 
-  const { x, y } = useSpring({ x: props.node.x!, y: props.node.y! })
-  const yOffset = y.to(
-    [-10000, 10000],
-    [-10000 + labelOffset, 10000 + labelOffset]
-  )
+  const { x, y } = useSpring({ x: props.node.x, y: props.node.y })
 
   return (
     <>
-      <a.mesh visible position-x={x} position-y={y} position-z={-10}>
-        <circleBufferGeometry attach="geometry" args={[10, 16]} />
-        <meshBasicMaterial
-          attach="material"
-          color={props.active ? activeColor : inactiveColor}
-        />
-      </a.mesh>
-      <Label
+      <animated.text
+        fill={props.active ? activeColor : inactiveColor}
         x={x}
-        y={yOffset}
-        color={props.active ? activeColor : inactiveColor}
-        text={props.node.label.replace(/_*/g, '')}
+        y={y.interpolate((y) => y! - labelOffset)}
+        opacity={1}
+        fontSize={`${fontSize}px`}
+        textAnchor="middle"
+        alignmentBaseline="central"
+        onClick={onClick}
+      >
+        {props.node.label.replace(/_*/g, '')}
+      </animated.text>
+      <animated.circle
+        cx={x}
+        cy={y}
+        r={nodeSize}
+        opacity={1}
+        onClick={onClick}
+        fill={props.active ? activeColor : inactiveColor}
       />
     </>
   )
-  // return (
-  //   <>
-  //     <a.mesh visible position-x={x} position-y={y} position-z={-10}>
-  //       <circleBufferGeometry attach="geometry" args={[10, 16]} />
-  //       <meshBasicMaterial
-  //         attach="material"
-  //         color={props.active ? activeColor : inactiveColor}
-  //       />
-  //     </a.mesh>
-  //     <a.mesh visible position-x={x} position-y={yOffset} position-z={-8}>
-  //       <textBufferGeometry
-  //         args={[
-  //           props.node.label.replace(/_*/g, ''),
-  //           {
-  //             font: FONT,
-  //             height: 1,
-  //             size: 10,
-  //           },
-  //         ]}
-  //         attach="geometry"
-  //       />
-  //       <meshBasicMaterial
-  //         attach="material"
-  //         color={props.active ? activeColor : inactiveColor}
-  //       />
-  //     </a.mesh>
-  //   </>
-  // )
 }
 
 export default Node
